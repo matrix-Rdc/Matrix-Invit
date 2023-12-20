1c9df09865bfbeb74a9fcf805fd00f5907d39309
jeffren <jeffrenmokamo726@gmail.com>
2023-12-20 13:21:44 +0100

    Finalisation de la page d'accueil

--- javascript/invit.js
+++ javascript/invit.js
@@ -1,12 +1,52 @@
 const divToggle = document.querySelector('#div-toggle');
 const Toggle = document.querySelector('#toggle');
 
+const btnNext = document.querySelector('.btn-next');
+
+var mds;
+var ctr = 0;
+
+btnNext.onclick = () => {
+
+    ctr++;
+    document.querySelector('#txt-acc').innerText = "Créer des invités";
+
+    if(ctr > 1){
+        document.querySelector('#txt-acc').innerText = "Participer à un événement";
+        btnNext.innerText = "Se connecter";
+    }
+    if(ctr > 2){
+        document.querySelector('.link-conn').setAttribute("href", "login.html");
+    }
+
+}
+
 divToggle.onclick = () => {
 
+    divToggle.classList.toggle('bg-sky');
     Toggle.classList.toggle('translate');
     if(Toggle.classList.contains('translate')){
         document.body.style.backgroundColor = '#082032';
         document.body.style.color = "#fff";
+        localStorage.setItem('mds', true);
+    }
+    else{
+        document.body.style.backgroundColor = '#fff';
+        document.body.style.color = "#000";
+        localStorage.removeItem('mds');
+    }
+
+}
+
+window.onload = () => {
+
+    mds = localStorage.getItem('mds');
+
+    if(mds){
+        document.body.style.backgroundColor = '#082032';
+        document.body.style.color = "#fff";
+        divToggle.classList.add('bg-sky');
+        Toggle.classList.add('translate');
     }
 
 }
